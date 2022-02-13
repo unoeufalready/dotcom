3e37d0bd9eb74b5c9767034aab82b510f6722b9f
Dillon Christensen <j.dillon.christensen@gmail.com>
2022-02-12 21:55:56 -0600

    added privacy policy

---
+++ src/PrivacyPolicy.tsx
@@ -0,0 +1,23 @@
+export default function PrivacyPolicy() {
+  return (
+    <>
+      <h1>Unoeufalready Privacy Policy</h1>
+      <p>
+        No user data is collected or transmitted to/by Unoeufalready from the
+        following products we provide:
+      </p>
+      <ul>
+        <li>Halftime - Google Play Store</li>
+        <li>This website</li>
+        <li>Simpson, Homer, that is all.</li>
+      </ul>
+      <h2>Are you sure though?</h2>
+      <p>
+        Yes! I have nowhere to put it anyway. That requires a backend and
+        thought. I'm just building clients right now. I don't know what your
+        browser tracks or what Google Play Store tracks from you downloading an
+        app. But I'm not taking any data. Woo hoo!
+      </p>
+    </>
+  );
+}
